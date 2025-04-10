4f3fa2cac79c81f1b5b8c87a802df91dbef27344
AlsabilaSP <alsabila.shakina@gmail.com>
2025-04-10 18:24:12 +0700

    init html files and folder structures

---
+++ src/js/core/navbar.js
@@ -0,0 +1,20 @@
+const navbarHTML = `
+<nav class="bg-gray-800">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="flex flex-1 items-center justify-start">
+          <div class="ml-6 block">
+            <div class="flex space-x-4">
+              <a href="posts.html" id="posts-link" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Posts</a>
+              <a href="reports.html" id="reports-link" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Reports</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</nav>
+`;
+
+// Inject the navbar HTML
+const body = document.querySelector('body');
+body.insertAdjacentHTML('afterbegin', navbarHTML);
